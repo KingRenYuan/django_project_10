0cdfae49cf8dc1f66faadc5ea5bdc1a7441b5394
RenHongYuan <56792063+KingRenYuan@users.noreply.github.com>
2023-10-18 20:42:10 +0800

    Update urls.py
    urls update

--- django_project/users/urls.py
+++ django_project/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.log_in, name='login'),
+    path('login/', views.sign_in, name='login'),
     path('logout/', views.log_out, name='logout'),
     path('register/', views.sign_up, name='register'),
 ]
